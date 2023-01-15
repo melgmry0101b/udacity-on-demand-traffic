cab3591733e660b5750b73797edfbaed36e18ee8
Mohammed Elghamry <44609329+melgmry0101b@users.noreply.github.com>
2023-01-15 19:12:56 +0200

    feat: implement led

--- src/UdacityOnDemandTraffic/ECUAL/LED/led.c
+++ src/UdacityOnDemandTraffic/ECUAL/LED/led.c
@@ -7,3 +7,54 @@
 
 #include "../../framework.h"
 #include "led.h"
+
+/************************************************************************/
+/* LED Methods                                                          */
+/************************************************************************/
+
+// =======================================
+// Initialize a pin for driving LED.
+// =======================================
+void LED_init(PIN_PORT port, uint8_t pin)
+{
+	DIO_init(port, pin, PIN_MODE_OUT);
+}
+
+// =======================================
+// Turn LED on.
+// =======================================
+void LED_on(PIN_PORT port, uint8_t pin)
+{
+	DIO_write(port, pin, PIN_STATUS_HIGH);	
+}
+
+// =======================================
+// Turn LED off.
+// =======================================
+void LED_off(PIN_PORT port, uint8_t pin)
+{
+	DIO_write(port, pin, PIN_STATUS_LOW);
+}
+
+// =======================================
+// Toggle LED.
+// =======================================
+void LED_toggle(PIN_PORT port, uint8_t pin)
+{
+	DIO_toggle(port, pin);
+}
+
+// =======================================
+// Blink LED.
+// =======================================
+void LED_blink(PIN_PORT port, uint8_t pin, uint16_t cycle_ms, uint16_t cycles)
+{
+	if (cycles == 0) { return; }
+	if (cycle_ms == 0) { return; }
+	
+	while (cycles-- > 0)
+	{
+		LED_toggle(port, pin);
+		sleep(cycle_ms);	
+	}
+}
