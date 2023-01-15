05e4e74973e480ba12e0e299c165b382267beebd
Mohammed Elghamry <44609329+melgmry0101b@users.noreply.github.com>
2023-01-15 16:53:03 +0200

    feat: implement button

--- src/UdacityOnDemandTraffic/ECUAL/Button/button.c
+++ src/UdacityOnDemandTraffic/ECUAL/Button/button.c
@@ -7,3 +7,46 @@
 
 #include "../../framework.h"
 #include "button.h"
+
+/************************************************************************/
+/* Globals                                                              */
+/************************************************************************/
+
+static BUTTON_CALLBACK g_buttonInt0Callback = NULL;
+
+/************************************************************************/
+/* ISR                                                                  */
+/************************************************************************/
+
+ISR(INT0_vect)
+{
+	if (g_buttonInt0Callback)
+	{
+		(*g_buttonInt0Callback)();
+	}
+}
+
+/************************************************************************/
+/* Buttons methods                                                      */
+/************************************************************************/
+
+// =======================================
+// Initialize button on INT0.
+// =======================================
+void BUTTON_INT0_init(void)
+{
+	// Set interrupt mode on INT0 to falling edge -when the button is released-
+	// NOTE: we are `OR`ing with the value of MCUCR avoiding changes on other bits.
+	MCUCR |= (1 << ISC01) | (1 << ISC00);
+	
+	// Enable INT0
+	SET_BIT(GICR, INT0);
+}
+
+// =======================================
+// Set interrupt callback.
+// =======================================
+void BUTTON_INT0_set_callback(BUTTON_CALLBACK callback)
+{
+	g_buttonInt0Callback = callback;
+}
